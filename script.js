15829c170a9d84b6be82354d01ef434bebf653eb
Denis <logunovich@gmail.com>
2021-08-27 18:30:33 +0200

    ver 1.1

--- script.js
+++ script.js
@@ -1,76 +1,116 @@
 'use strict';
 
-// Ver 1.6 commit: Logunovich Denis (logunovich@gmail.com)
+// Ver 1.1 commit: Logunovich Denis (logunovich@gmail.com)
 
 const btnStart = document.querySelector('.form__button-start'),
       btnPause = document.querySelector('.form__button-pause'),
+      btnFinish = document.querySelector('.form__button-finish'), 
       hoursToday = document.querySelector('#hours-today'),
       minutesToday = document.querySelector('#minutes-today'),
       secondsToday = document.querySelector('#seconds-today'),
       hoursAll = document.querySelector('#hours-all'),
-      minutesAll = document.querySelector('#minutes-all');
+      minutesAll = document.querySelector('#minutes-all'),
+      films = document.querySelector('#films');
 
-let myTime;
-let seconds;
-let oldTime = 0;
-let newTime;
-let time;
-let first = true;
-let isPause = true;
+let myTime,
+    isPause = true;
 
-hoursToday.textContent = `00:`;
-minutesToday.textContent = `00:`;
-secondsToday.textContent = `00`;
-hoursAll.textContent = `00h`;
-minutesAll.textContent = `00m`;
+btnPause.style.cssText = 'background-color: #b3eac0';
+updTodayTime(+localStorage.getItem('allSeconds'));
+updAllTime(+localStorage.getItem('allTime'));
 
-function timer (time) {
-    myTime = setInterval(() => {
-        newTime = (new Date).getTime();
-        seconds = (Math.round((newTime - time)/1000)) + oldTime;
-
-        let showTime = {
-            seconds: seconds%60,
-            minutes: Math.floor(seconds/60)%60,
-            hours: Math.floor(seconds/3600)
+function updAllTime (sec) {
+    let showTime = {
+        minutes: Math.floor(sec/60)%60,
+        hours: Math.floor(sec/3600)
     };
     if (showTime.minutes < 10) {
-        showTime.minutes = `0${showTime.minutes}`
+    showTime.minutes = `0${showTime.minutes}`
     }
-
-    if (showTime.seconds < 10) {
-        showTime.seconds = `0${showTime.seconds}`
-    }
-
+    
     if (showTime.hours < 10) {
-        showTime.hours = `0${showTime.hours}`
+    showTime.hours = `0${showTime.hours}`
     }
+    
+    hoursAll.textContent = `${showTime.hours}h`;
+    minutesAll.textContent = `${showTime.minutes}m`;
 
-        hoursToday.textContent = `${showTime.hours}:`;
-        minutesToday.textContent = `${showTime.minutes}:`;
-        secondsToday.textContent = `${showTime.seconds}`;
-        // mSecondsToday.textContent = `${showTime.mSeconds}:`;
-        
+    const countFilms = Math.floor(+localStorage.getItem('allTime') / 8520);
 
+    films.textContent = countFilms;
+}
+
+function updTodayTime (sec) {
+    let showTime = {
+        seconds: sec%60,
+        minutes: Math.floor(sec/60)%60,
+        hours: Math.floor(sec/3600)
+    };
+    if (showTime.minutes < 10) {
+    showTime.minutes = `0${showTime.minutes}`
+    }
+    
+    if (showTime.seconds < 10) {
+    showTime.seconds = `0${showTime.seconds}`
+    }
+    
+    if (showTime.hours < 10) {
+    showTime.hours = `0${showTime.hours}`
+    }
+    
+    hoursToday.textContent = `${showTime.hours}:`;
+    minutesToday.textContent = `${showTime.minutes}:`;
+    secondsToday.textContent = `${showTime.seconds}`;
+}
+
+function timer () {
+    myTime = setInterval(() => {
+        localStorage.setItem('allSeconds', +(localStorage.getItem('allSeconds')) + 1);
+        updTodayTime(+localStorage.getItem('allSeconds'));
     }, 1000);
 }
 
     btnStart.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    time = (new Date).getTime();    
-    if (isPause) {
-    isPause = false;
-    timer(time);
+        e.preventDefault(); 
+        btnStart.style.cssText = 'background-color: #b3eac0';
+        btnPause.style.cssText = '';
+        if (isPause) {
+            isPause = false;
+            timer();
     }  
 });
 
     btnPause.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    if (!isPause) {
-        clearInterval(myTime);
-        isPause = true;
-        oldTime = seconds;
+        e.preventDefault(); 
+        btnStart.style.cssText = '';
+        btnPause.style.cssText = 'background-color: #b3eac0';
+        if (!isPause) {
+            clearInterval(myTime);
+            isPause = true;
     }
-
 });
 
+btnFinish.addEventListener('click', (e) => {
+    e.preventDefault();
+    clearInterval(myTime);
+    isPause = true;
+    btnStart.style.cssText = '';
+    btnPause.style.cssText = 'background-color: #b3eac0';
+
+    let finalTime = +localStorage.getItem('allTime') + +localStorage.getItem('allSeconds');
+
+    localStorage.setItem('allTime', finalTime);
+    localStorage.setItem('allSeconds', 0);
+    
+    updTodayTime(+localStorage.getItem('allSeconds'));
+    updAllTime(+localStorage.getItem('allTime'));
+})
+
+
+localStorage.setItem('allTime', 269800); // записываем какой-то ключ
+
+// localStorage.getItem('number'); // получаем какой-то ключ
+
+// localStorage.removeItem('number'); // удаляем какой-то ключ
+
+// localStorage.clear(); // полностью очищаем ключи в LocalStorage
